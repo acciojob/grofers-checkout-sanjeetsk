f6ed1d41748b9ee1ea4688789e9639e9b99d2d20
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-06-22 23:51:22 +0530

    final

--- script.js
+++ script.js
@@ -2,23 +2,34 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
-let ans=0;
+
 
 const getSum = () => {
 //Add your code here
+
+    let ans=0;
     price.forEach((e) => {
         ans += parseInt(e.textContent);
     })
 
+    
+
     const table = document.querySelector('table');
-    const row = document.createElement("tr");
+    const newRow = document.createElement("tr");
     const cell1 = document.createElement("td");
     cell1.textContent = "Total";
     const cell2 = document.createElement("td");
     cell2.textContent = ans;
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-    table.appendChild(row);
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+
+    let existingTotalRow = document.querySelector('#totalRow');
+    if(existingTotalRow){
+        existingTotalRow.remove();
+    }
+
+    newRow.id = "totalRow";
+    table.appendChild(newRow);
 
 };
 
